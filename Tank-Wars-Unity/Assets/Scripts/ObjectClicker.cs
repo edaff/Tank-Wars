8005fabb4d21be54b0ad34b156c39119f0e926a5
TylerAUhl <tyleruhl@ufl.edu>
2019-02-13 16:06:31 -0500

    Object clicker v1 made

--- Tank-Wars-Unity/Assets/Scripts/ObjectClicker.cs
+++ Tank-Wars-Unity/Assets/Scripts/ObjectClicker.cs
@@ -18,26 +18,8 @@ public class ObjectClicker : MonoBehaviour {
                 {
                     Rigidbody rb;
                     if (rb = hit.transform.GetComponent<Rigidbody>())
-                    {
-                    	objectClicked = hit.transform.gameObject;
-                    	print(hit.transform.gameObject);
-                    	/*
-                        //print(hit.transform.gameObject);
-                        if(hit.transform.gameObject.tag == "Tank"){
-                            tankClicked = hit.transform.gameObject;
-                        }
-                           else if(hit.transform.gameObject.tag == "Tile" && tankClicked != null){
-                               tileClicked = hit.transform.gameObject;
-                              //print("tankClicked: ");
-                              //print(tankClicked);
-                              //print("tileClicked: ");
-                              //print(tileClicked);
-                              translateVector = new Vector3(tileClicked.transform.position.x, 1f,tileClicked.transform.position.z);
-                              tankClicked.transform.position = translateVector;
-                              tileClicked = null;
-                              tankClicked = null;
-                           }
-                           */
+                    {                        
+                        objectClicked = hit.transform.gameObject;
                     }
                 }
             }

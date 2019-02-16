253e823a336e2383a5607dedcd181ad5981601ad
Daniel Frystacky <danielfrystacky@gmail.com>
2019-02-16 17:28:51 -0500

    Spawn a random tank in main menu screen to add livlyness

---
+++ Tank-Wars-Unity/Assets/Scripts/MainMenuTankSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuTankSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] tanksPreFabs;
+    [SerializeField] static GameObject tankSpawner;
+  
+    Vector3 tankSpawnLocation = new Vector3(5, 1, 9);
+
+
+    //will spawn a random tank for the camera to rotate around
+    void Start()
+    {
+        
+        Instantiate(tanksPreFabs[Random.Range(0, tanksPreFabs.Length-1)], transform.position, Quaternion.Euler(0, 180, 0));
+    }
+
+}

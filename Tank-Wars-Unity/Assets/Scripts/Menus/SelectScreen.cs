031b58da1cb9be1e0c2f46bad4f51062e832e962
frystacky <danielfrystacky@gmail>
2019-02-21 09:36:49 -0500

    playing 2 tank select half working

--- Tank-Wars-Unity/Assets/Scripts/Menus/SelectScreen.cs
+++ Tank-Wars-Unity/Assets/Scripts/Menus/SelectScreen.cs
@@ -20,6 +20,10 @@ public class SelectScreen : MonoBehaviour
     [SerializeField] GameObject p1Ready;
     [SerializeField] GameObject p1Redo;
 
+
+    [SerializeField] GameObject p2Ready;
+    [SerializeField] GameObject p2Redo;
+
     GameStatus currentState;
 
 
@@ -35,13 +39,22 @@ public class SelectScreen : MonoBehaviour
 
     void Update()
     {
+        //this will pop up redo? and ready GUI for player 1
         if(p1ArrayIdex == whatLevelAmI && !player1FishedPicking)
         {
             p1Redo.SetActive(true);
             p1Ready.SetActive(true);
         }
 
-        if(Input.GetKeyDown(KeyCode.Q))
+        //this will pop up redo? and ready GUI for player 2
+        if (p2ArrayIdex == whatLevelAmI && !player2FishedPicking)
+        {
+            p2Redo.SetActive(true);
+            p2Ready.SetActive(true);
+        }
+
+        //player 1 keys
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             Debug.Log("Q was hit");
             Player1Qpick();
@@ -62,9 +75,31 @@ public class SelectScreen : MonoBehaviour
             
         }
 
+        //player 2 keys
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("keypad1 was hit");
+            Player2KeyPad1pick();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Debug.Log("keypad2 was hit");
+            Player2KeyPad2pick();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            Debug.Log("keypad3 was hit");
+            Player2KeyPad3pick();
+
+        }
+
 
     }
-
+    //player1 pick functions
     private void Player1Qpick()
     {
         if(whatLevelAmI > p1ArrayIdex) 
@@ -107,7 +142,54 @@ public class SelectScreen : MonoBehaviour
             return;
         }
     }
+    //end of player 1 functions
 
+    //player 2 pick functions
+    private void Player2KeyPad1pick()
+    {
+        if (whatLevelAmI > p2ArrayIdex)
+        {
+            player2TankPicks[p2ArrayIdex] = 1;
+            p2ArrayIdex++;
+            return;
+        }
+        else
+        {
+
+            return;
+        }
+    }
+
+    private void Player2KeyPad2pick()
+    {
+        if (whatLevelAmI > p2ArrayIdex)
+        {
+            player2TankPicks[p2ArrayIdex] = 2;
+            p2ArrayIdex++;
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void Player2KeyPad3pick()
+    {
+        if (whatLevelAmI > p2ArrayIdex)
+        {
+            player2TankPicks[p2ArrayIdex] = 3;
+            p2ArrayIdex++;
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+    //end of player 2 functions
+
+    //player 1 GUI control functions
     public void Player1RedoPick()
     {
         p1Redo.SetActive(false);
@@ -122,7 +204,24 @@ public class SelectScreen : MonoBehaviour
         player1FishedPicking = true;
         currentState.SetPlayer1TanksPick(player1TankPicks);
     }
+    //end of player 1 GUI control functions
 
+    //player 1 GUI control functions
+    public void Player2RedoPick()
+    {
+        p2Redo.SetActive(false);
+        p2Ready.SetActive(false);
+        p1ArrayIdex = 0;
+    }
+
+    public void p2DonePicking()
+    {
+        p2Ready.SetActive(false);
+        p2Redo.SetActive(false);
+        player2FishedPicking = true;
+        currentState.SetPlayer2TanksPick(player2TankPicks);
+    }
+    //end of player 1 GUI control functions
 
 
 

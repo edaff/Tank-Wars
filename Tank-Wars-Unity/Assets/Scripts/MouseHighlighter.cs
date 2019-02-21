d3d31b614aabe0be9b02fa7b6d1ac42edd81ce84
Josh Yates <magicdonuts12@gmail.com>
2019-02-20 19:30:33 -0500

    Blocks are highlighted when the mouse is over it

---
+++ Tank-Wars-Unity/Assets/Scripts/MouseHighlighter.cs
@@ -0,0 +1,30 @@
+﻿// Source: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnMouseOver.html
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHighlighter : MonoBehaviour
+{
+    Color m_MouseOverColor = Color.blue;
+    Color m_OriginalColor;
+    MeshRenderer m_Renderer;
+
+    void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_Renderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        // Change the color of the GameObject when the mouse is over GameObject
+        m_Renderer.material.color = m_MouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        // Reset the color of the GameObject back to normal
+        m_Renderer.material.color = m_OriginalColor;
+    }
+}

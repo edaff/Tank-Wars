3d0c7d289c71632b52340ceb5e4a0b6695cf5ed5
Daniel Frystacky <danielfrystacky@gmail.com>
2019-02-22 12:39:18 -0500

    added game over function in GameStatus

--- Tank-Wars-Unity/Assets/Scripts/GameStatus.cs
+++ Tank-Wars-Unity/Assets/Scripts/GameStatus.cs
@@ -11,6 +11,11 @@ public class GameStatus : MonoBehaviour
     [SerializeField] int[] player1TankPicks;                        //holds players 1 picks
     [SerializeField] int[] player2TankPicks;                        //holds players 1 picks
 
+    [Header("Which player won")]
+    [SerializeField] bool player1Wins = false;
+    [SerializeField] bool player2Wins = false;
+
+
     SceneLoader nextLevel;
 
     void Start()
@@ -37,10 +42,23 @@ public class GameStatus : MonoBehaviour
         this.player2TankPicks = player2TankPicks;
     }
 
+    public void SetPlayer1Win()
+    {
+        player1Wins = true;
+        nextLevel.LoadGameOverScreen();
+    }
+
+    public void SetPlayer2Win()
+    {
+        player2Wins = true;
+        nextLevel.LoadGameOverScreen();
+    }
+
     //gets Difficuiry from start screen
     public int GetDifficuity()
     {
         return this.difficuityInfo;
     }
+
 
 }

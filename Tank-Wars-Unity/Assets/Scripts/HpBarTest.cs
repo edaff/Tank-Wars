cb718fa2ac19821993bdf931fa2158889d0d93a6
Daniel Frystacky <danielfrystacky@gmail.com>
2019-02-25 17:01:29 -0500

    added a test hp bar to game over screen

---
+++ Tank-Wars-Unity/Assets/Scripts/HpBarTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBarTest : MonoBehaviour
+{
+    [SerializeField] private HpBar hpbar;
+    [SerializeField] float hp;
+    [SerializeField] int loadTime = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hpbar = FindObjectOfType<HpBar>();
+        hp = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(hp >= 0f)
+        {
+            Debug.Log("Im in it");
+            StartCoroutine(MinsHPBar());
+
+        }*/
+    }
+
+    IEnumerator MinsHPBar()
+    {
+        hpbar.SetSize(hp);
+        hp -= 0.1f;
+        Debug.Log("My hp is" + hp);
+        yield return new WaitForSeconds(loadTime);
+
+
+    }
+
+
+
+
+}

95f9c85fda42d6ee7126538964e51c64b634c40b
Josh Yates <magicdonuts12@gmail.com>
2019-03-03 03:06:15 -0500

    Added free cam

--- Tank-Wars-Unity/Assets/Scripts/Camera/CameraAngles.cs
+++ Tank-Wars-Unity/Assets/Scripts/Camera/CameraAngles.cs
@@ -4,117 +4,57 @@ using UnityEngine;
 
 public class CameraAngles : MonoBehaviour
 {
-    public int select = 0;
-    public bool birdCam = false;
-    public bool angleCam = false;
-    public Player player;
+    public Vector3 middle;
+    private float initialFOV;
+    public float minZoomLimit = .1f;
+    public float maxZoomLimit = 10f;
+    public float distance = 0f;
+    private float xRotate;
+    private float yRotate;
+    public float xRotateSpeed = 3.0f;
+    public float yRotateSpeed = 1.5f;
+    public float maxYRotate = 90.0f;
+    public float minYRotate = 0.0f;
+    public float xMoveSpeed = 100.0f;
+    public float zMoveSpeed = 100.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.transform.position = new Vector3(4.5f, 10f, 4.5f);
-        Camera.main.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        middle = new Vector3(4.5f, 0f, 4.5f);
+        Vector3 angles = transform.eulerAngles;
+        xRotate = angles.x;
+        yRotate = angles.y + 45;
+        Camera.main.fieldOfView += Camera.main.fieldOfView - 115;
+        initialFOV = Camera.main.fieldOfView;
+        transform.position = new Vector3(0, 0, -distance) + middle;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once at the end of a frame
+    void LateUpdate()
     {
-        if (select == 4)
+        if (Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift))
         {
-            select = 0;
+            float zoom = Camera.main.fieldOfView - Input.GetAxis("Vertical") * yRotateSpeed;
+            if (zoom >= initialFOV / maxZoomLimit && zoom <= initialFOV / minZoomLimit)
+            {
+                Camera.main.fieldOfView -= Input.GetAxis("Vertical") * yRotateSpeed;
+            }
+        }
+        else
+        {
+            xRotate += Input.GetAxis("Horizontal") * xRotateSpeed;
+            yRotate += Input.GetAxis("Vertical") * yRotateSpeed;
+            if (yRotate > 360) yRotate -= 360;
+            if (yRotate < -360) yRotate += 360;
+            yRotate = Mathf.Clamp(yRotate, minYRotate, maxYRotate);
         }
 
-        if (birdCam || angleCam)
-        {
-            if (Input.GetKeyDown(KeyCode.Tab))
-            {
-                select++;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1) && birdCam == false && angleCam == false)
-        {
-            birdCam = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha1) && birdCam == true)
-        {
-            birdCam = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2) && angleCam == false && birdCam == false)
-        {
-            angleCam = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && angleCam == true)
-        {
-            angleCam = false;
-        }
-
-        if (birdCam)
-        {
-            if (select == 0)
-            {
-                Camera.main.transform.position = new Vector3(4.5f, 10f, 4.5f);
-                Camera.main.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
-            }
-            else if (select == 1)
-            {
-                Camera.main.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
-            }
-            else if (select == 2)
-            {
-                Camera.main.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
-            }
-            else
-            {
-                Camera.main.transform.rotation = Quaternion.Euler(90f, 270f, 0f);
-            }
-        }
-
-        else if (angleCam)
-        {
-            if (select == 0)
-            {
-                Camera.main.transform.position = new Vector3(9.5f, 9f, -0.5f);
-                Camera.main.transform.rotation = Quaternion.Euler(60f, -45f, 0f);
-            }
-            else if (select == 1)
-            {
-                Camera.main.transform.position = new Vector3(9.5f, 9f, 9.5f);
-                Camera.main.transform.rotation = Quaternion.Euler(60f, -135f, 0f);
-            }
-            else if (select == 2)
-            {
-                Camera.main.transform.position = new Vector3(-0.5f, 9f, 9.5f);
-                Camera.main.transform.rotation = Quaternion.Euler(60f, 135f, 0f);
-            }
-            else
-            {
-                Camera.main.transform.position = new Vector3(-0.5f, 9f, -0.5f);
-                Camera.main.transform.rotation = Quaternion.Euler(60f, 45f, 0f);
-            }
-        }
-    }
-
-
-    // get the camera to rotate around the center of the map at any 
-    // camera position
-    void rotateCamAtCurrentPosition()
-    {
-        transform.Rotate(0, -Input.GetAxis("Mouse X") * 50, 0);
-    }
-
-    // needs to follow the tank that is clicked only if its the current players tank
-    void followPlayerCam()
-    {
-        if (player.getPlayerColor() == PlayerColors.Red)
-        {
-            //  player.getPlayerTanks()[0];
-        }
-        else if (player.getPlayerColor() == PlayerColors.Blue)
-        {
-
-        }
+        var rotation = Quaternion.Euler(yRotate, xRotate, 0);
+        var position = rotation * (new Vector3(0, 0, -distance)) + middle;
+        transform.rotation = rotation;
+        transform.position = position;
     }
 }
 

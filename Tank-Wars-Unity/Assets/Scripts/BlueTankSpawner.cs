8da9c1f0b54013f4368122469b389b99ff4fefd9
Daniel Frystacky <danielfrystacky@gmail.com>
2019-03-06 10:48:49 -0500

    added object to spawn tanks according to picks

---
+++ Tank-Wars-Unity/Assets/Scripts/BlueTankSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueTankSpawner : MonoBehaviour
+{
+    GameStatus currentState;
+    [SerializeField] int whatLevelAmI = 1;
+
+    [SerializeField] private GameObject[] redTanksPreFabs;
+    [SerializeField] public int[] getPicks;
+
+    Vector3 tankSpawnLocation = new Vector3(5, 1, 9);               //this is the spawn location for the first normal tank since its heigh is not the same as the others
+    Vector3 tankSpawnLocation2 = new Vector3(5, .8f, 9);            //this spawns location for the sinper and mortar tank
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = FindObjectOfType<GameStatus>();
+        whatLevelAmI = currentState.GetDifficuity();
+
+        getPicks = new int[] { 0, 0, 0 };
+        getPicks = currentState.getPlayer1TankPicks();
+
+        if ((getPicks[0] - 1) == 0)
+        { Instantiate(redTanksPreFabs[(getPicks[0] - 1)], tankSpawnLocation, Quaternion.Euler(0, 180, 0)); }
+        else { Instantiate(redTanksPreFabs[(getPicks[0] - 1)], tankSpawnLocation2, Quaternion.Euler(0, 180, 0)); }
+
+    }
+}
---
+++ Tank-Wars-Unity/Assets/Scripts/RedTankSpawner1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedTankSpawner1 : MonoBehaviour
+{
+
+    GameStatus currentState;
+    [SerializeField] int whatLevelAmI = 1;
+
+    [SerializeField] private GameObject[] redTanksPreFabs;
+    [SerializeField] public int[] getPicks;
+
+    Vector3 tankSpawnLocation = new Vector3(4, 1, 0);               //this is the spawn location for the first normal tank since its heigh is not the same as the others
+    Vector3 tankSpawnLocation2 = new Vector3(4, .8f, 0);            //this spawns location for the sinper and mortar tank
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = FindObjectOfType<GameStatus>();
+        whatLevelAmI = currentState.GetDifficuity();
+
+        getPicks = new int[] {0,0,0};
+        getPicks = currentState.getPlayer1TankPicks();
+
+        if((getPicks[0] - 1) == 0)
+        {Instantiate(redTanksPreFabs[(getPicks[0] - 1)], tankSpawnLocation, Quaternion.Euler(0, 0, 0));}
+        else{Instantiate(redTanksPreFabs[(getPicks[0] - 1)], tankSpawnLocation2, Quaternion.Euler(0, 0, 0)); }
+        
+
+    }
+}
